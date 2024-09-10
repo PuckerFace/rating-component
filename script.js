f13d4df42f78bb594bd20be4aa35695b9d374b39
PuckerFace <113001532+PuckerFace@users.noreply.github.com>
2024-09-10 21:16:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const rating = document.querySelectorAll('.btn');
+const submit = document.getElementById('submit');
+const ratedCard = document.getElementById('rated-card');
+const ratingCard = document.getElementById('rating-card');
+const finalRating = document.getElementById('final-rating');
+
+for (let i = 0; i < rating.length; i++) {
+  rating[i].addEventListener('click', (e) => {
+    console.log('clicked');
+    let score = i + 1;
+    console.log(score);
+    finalRating.innerHTML = `You selected ${score} out of 5`;
+    // rating[i].style.backgroundColor = 'white';
+    // rating[i].style.color = 'black';
+    let button = e.currentTarget;
+    rating.forEach((btn) => btn !== button && btn.classList.remove('white'));
+    button.classList.toggle('white');
+    // rating[i].classList.toggle('white');
+  });
+}
+submit.addEventListener('click', () => {
+  ratingCard.classList.toggle('hidden');
+  ratedCard.classList.toggle('hidden');
+});
